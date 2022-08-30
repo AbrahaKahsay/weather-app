6e3ff3c56b5113bdaa68d53c0a4edc0d9efd9034
Abraha Kahsay <abrahak81@gmail.com>
2022-08-30 10:37:53 +0300

    Set routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/city" element={<Deatils />} />
+      </Routes>
+    </>
+
   );
 }
 
