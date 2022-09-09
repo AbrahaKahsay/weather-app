718bae4598650f692818155bfeaf9fd7b82ae9d7
Abraha Kahsay <abrahak81@gmail.com>
2022-09-09 11:33:40 +0300

    Remove redux directory and components to a completly implement on React

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
+import './App.css';
 import React from 'react'
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Details from './components/Details';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/City" element={<Details />} />
-      </Routes>
-    </>
-  );
+    <div className='app-container'>
+      <div>
+        <div className='top'>
+          <div className='location'>
+            <p>Jimma</p>
+          </div>
+          <div className='temp'>
+            <h1>298K</h1>
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+       </div>
+       <div className='bottom'>
+          <div className='feels'>
+            <p>298K</p>
+          </div>
+          <div className='humudity'>
+            <p>20%</p>
+          </div>
+          <div className='wind'>
+            <p>12 MPH</p>
+          </div>
+        </div>
+      </div>
+   </div>   
+  )
 }
 
-export default App;
+export default App
