2837ba3c2ec6c6be56f622e44e55a1f86113bf86
Abraha Kahsay <abrahak81@gmail.com>
2022-09-09 23:43:42 +0300

    Add search and functionalities

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ function App() {
 
   const apiKey = '6ba82cfd5a63c82e9583996928fd44ff';
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=6ba82cfd5a63c82e9583996928fd44ff`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=6ba82cfd5a63c82e9583996928fd44ff`
 
   const searchLocation = (event) => {
     if(event.key === 'Enter'){
       axios.get(url)
-    .then((response) => {
+      .then((response) => {
       setData(response.data)
       console.log(response.data)
+      setLocation('')
     })
     } 
   } 
@@ -34,29 +35,34 @@ function App() {
       <div className='app'>
         <div className='top'>
           <div className='location'>
-            <p>Jimma</p>
+            <p>{data.name}</p>
           </div>
           <div className='temp'>
-            <h1>298K</h1>
+            {data.main? <h1>{data.main.temp} °C</h1> : null}
           </div>
           <div className='description'>
-            <p>Clouds</p>
+            {data.weather? <p>{data.weather[0].main}</p> : null }
+            {data.weather? <p>{data.weather[0].description}</p> : null }
           </div>
        </div>
+
+       {data.name !=undefined &&
        <div className='bottom'>
-          <div className='feels'>
-            <p className='bold'>298K</p>
-            <p>Feels Like</p>
-          </div>
-          <div className='humudity'>
-            <p className='bold'>20%</p>
-            <p>Humidity</p>
-          </div>
-          <div className='wind'>
-            <p className='bold'>12 MPH</p>
-            <p>Wind Speed</p>
-          </div>
-        </div>
+       <div className='feels'>
+         {data.main?<p className='bold'>{data.main.feels_like} °C</p>:null}
+         <p>Feels Like</p>
+       </div>
+       <div className='humidity'>
+       {data.main?<p className='bold'>{data.main.humidity} %</p>:null}
+         <p>Humidity</p>
+       </div>
+       <div className='wind'>
+       {data.wind?<p className='bold'>{data.wind.speed} MPH</p>:null}
+         <p>Wind Speed</p>
+       </div>
+     </div>
+       }
+
       </div>
    </div>   
   )
